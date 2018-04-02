4b54276634524365d43792ee33d911bc822c968d
Vladimir Alefirov <31669425+Alifa00@users.noreply.github.com>
2018-04-02 14:57:16 +0300

    Update matrix.cpp

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -1,11 +1,10 @@
 #include <catch.hpp>
 #include <sstream>
-
 #include "matrix.hpp"
 
 TEST_CASE("creating matrix")
 {
-    matrix_t matrix;
+    matrix_t<int> matrix;
     REQUIRE( matrix.rows() == 0 );
     REQUIRE( matrix.collumns() == 0 );
 }
@@ -17,8 +16,8 @@ TEST_CASE("reading matrix")
         "1 1 1\n"
         "2 2 2\n"
         "3 3 3" };
-    
-    matrix_t matrix;
+   
+    matrix_t<int> matrix;
     std::istringstream istream{ input };
     
     REQUIRE( matrix.read( istream ) );
@@ -30,6 +29,7 @@ TEST_CASE("reading matrix")
     
     REQUIRE( input == ostream.str() );
 }
+
 TEST_CASE("operation+")
 {
     std::string input{
@@ -48,22 +48,23 @@ TEST_CASE("operation+")
         "3 3 3\n"
         "3 3 3" };
     
-   
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
-     std::istringstream istream1{ input1 };
+    std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
     REQUIRE( matrix.rows() == 3 );
     REQUIRE( matrix.collumns() == 3 );
     REQUIRE( matrix1.read( istream1 ) );
     REQUIRE( matrix1.rows() == 3 );
     REQUIRE( matrix1.collumns() == 3 );
-    matrix=matrix+matrix1;
+    REQUIRE_NOTHROW(matrix=matrix+matrix1); 
+    
     std::ostringstream ostream;
     matrix.write( ostream );
     
     REQUIRE( input2 == ostream.str() );
 }
+
 TEST_CASE("operation-")
 {
     std::string input{
@@ -82,8 +83,7 @@ TEST_CASE("operation-")
         "1 1 1\n"
         "1 1 1" };
     
-   
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
     std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
@@ -92,12 +92,14 @@ TEST_CASE("operation-")
     REQUIRE( matrix1.read( istream1 ) );
     REQUIRE( matrix1.rows() == 3 );
     REQUIRE( matrix1.collumns() == 3 );
-    matrix=matrix-matrix1;
+    REQUIRE_NOTHROW(matrix=matrix-matrix1); 
+    
     std::ostringstream ostream;
     matrix.write( ostream );
     
     REQUIRE( input2 == ostream.str() );
 }
+
 TEST_CASE("operation+=")
 {
     std::string input{
@@ -115,23 +117,24 @@ TEST_CASE("operation+=")
         "3 3 3\n"
         "3 3 3\n"
         "3 3 3" };
-    
    
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
-     std::istringstream istream1{ input1 };
+    std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
     REQUIRE( matrix.rows() == 3 );
     REQUIRE( matrix.collumns() == 3 );
     REQUIRE( matrix1.read( istream1 ) );
     REQUIRE( matrix1.rows() == 3 );
     REQUIRE( matrix1.collumns() == 3 );
-    matrix+=matrix1;
+    REQUIRE_NOTHROW(matrix+=matrix1); 
+    
     std::ostringstream ostream;
     matrix.write( ostream );
     
     REQUIRE( input2 == ostream.str() );
 }
+
 TEST_CASE("operation-=")
 {
     std::string input{
@@ -150,8 +153,7 @@ TEST_CASE("operation-=")
         "1 1 1\n"
         "1 1 1" };
     
-   
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
     std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
@@ -160,12 +162,14 @@ TEST_CASE("operation-=")
     REQUIRE( matrix1.read( istream1 ) );
     REQUIRE( matrix1.rows() == 3 );
     REQUIRE( matrix1.collumns() == 3 );
-    matrix-=matrix1;
+    REQUIRE_NOTHROW(matrix-=matrix1); 
+    
     std::ostringstream ostream;
     matrix.write( ostream );
     
     REQUIRE( input2 == ostream.str() );
 }
+
 TEST_CASE("operation*")
 {
     std::string input{
@@ -183,9 +187,8 @@ TEST_CASE("operation*")
         "6 6 6\n"
         "6 6 6\n"
         "6 6 6" };
-    
    
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
     std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
@@ -194,12 +197,14 @@ TEST_CASE("operation*")
     REQUIRE( matrix1.read( istream1 ) );
     REQUIRE( matrix1.rows() == 3 );
     REQUIRE( matrix1.collumns() == 3 );
-    matrix*=matrix1;
+    REQUIRE_NOTHROW(matrix*=matrix1);
+    
     std::ostringstream ostream;
     matrix.write( ostream );
     
     REQUIRE( input2 == ostream.str() );
 }
+
 TEST_CASE("operation=")
 {
     std::string input{
@@ -212,9 +217,8 @@ TEST_CASE("operation=")
         "1 1 \n"
         "1 1 \n"
         "1 1 " };
-    
    
-    matrix_t matrix,matrix1;
+    matrix_t<int> matrix,matrix1;
     std::istringstream istream{ input };
     std::istringstream istream1{ input1 };
     REQUIRE( matrix.read( istream ) );
@@ -228,4 +232,39 @@ TEST_CASE("operation=")
     matrix.write( ostream );
     
     REQUIRE( input1 == ostream.str() );
+}
+
+TEST_CASE("typetests"){
+     std::string input{
+        "3, 3\n"
+        "1 1 1\n"
+        "2 2 2\n"
+        "3 3 3" };
+   
+    matrix_t<int> matrix;
+    matrix_t<float> matrix1;
+    matrix_t<double> matrix2;
+    std::istringstream istream{ input };
+    std::istringstream istream1{ input };
+    std::istringstream istream2{ input };
+    
+    REQUIRE( matrix.read( istream ) );
+    REQUIRE( matrix.rows() == 3 );
+    REQUIRE( matrix.collumns() == 3 );
+    REQUIRE( matrix1.read( istream1 ) );
+    REQUIRE( matrix1.rows() == 3 );
+    REQUIRE( matrix1.collumns() == 3 );
+    REQUIRE( matrix2.read( istream2 ) );
+    REQUIRE( matrix2.rows() == 3 );
+    REQUIRE( matrix2.collumns() == 3 );
+    
+    std::ostringstream ostream;
+    matrix.write( ostream );
+    ostream<<'\n';
+    matrix1.write(ostream);
+
+    ostream<<'\n';
+    matrix2.write(ostream);
+    
+    REQUIRE( input+'\n'+input+'\n'+input == ostream.str() );
 }
